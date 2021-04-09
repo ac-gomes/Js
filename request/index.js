3d01ccaaba14bff86083da6da4bbb090d2244693
ac-gomes <acgoms88@gmail.com>
2021-04-08 23:29:10 -0300

    load card function was separated from  filter function

--- request/index.js
+++ request/index.js
@@ -5,16 +5,19 @@ var tableEl = document.getElementById('table-content');
 var tbody = document.querySelector("#table-content");
 var cards = document.getElementsByClassName('card');
 var mediaCards = document.getElementsByClassName('mediaCard');
+var cardUserName = document.getElementById('name-card');
+var btnNext = document.getElementsByClassName('next');
+var btnPrevious = document.getElementsByClassName('previous');
 
-//Obter dados
+
 async function getData(url) {
     const response = await fetch(url);
     return await response.json();
 }
-
 var dataUsers = getData(dataSource);
 
-async function loadData(fn){
+//Initialize
+async function loadDataTable(fn){
   dataUsers = await fn;
   for (let user of dataUsers){
    row = `
@@ -28,20 +31,45 @@ async function loadData(fn){
         </tr>`
     tableEl.innerHTML += row
   }
+  loadDataCard(cards, mediaCards, dataUsers);
+
 }
 
-loadData(dataUsers)
+async function loadDataCard(cardsElements,mediaCardEl,data){
+  let UserData = await data;
+  let [{name,address,company}] = UserData;
 
-async function  loadDataCards(cardsElements,tableElement,mediaCardEl,dataUsers) {
-  const allData  = await dataUsers;
+  if (cardUserName.innerText == 'Titulo do Card'){
+    for (var card = 0; card < cardsElements.length; card++) {
+      if (cardsElements[card].id== 'company'){
+        document.getElementById('name-card2').innerHTML = company.name;
+        document.getElementById('catchPhrase').innerHTML = company.catchPhrase;
+        document.getElementById('bs').innerHTML = company.bs;
+      } else {
+        document.getElementById('name-card').innerHTML = name;
+        for (var media = 0; media < mediaCardEl.length; media++) {
+          if(mediaCardEl[media].id =='card-address'){
+            mediaCardEl[media].children[0].innerHTML = address.street
+            mediaCardEl[media].children[1].innerHTML = address.suite
+            mediaCardEl[media].children[2].innerHTML = address.city
+            mediaCardEl[media].children[3].innerHTML = address.zipcode
+          }
+        }
+      }
+    }
+  };
+}
 
+loadDataTable(dataUsers);
+
+async function filterUserbyClick(tableElement,cardsElements,mediaCardEl,data){
+  const UserData = await data
   tableElement.addEventListener('click', el => {
     const rowValue = el.target.parentNode.innerText[0]
 
-    const [{id,name,address,company}]= allData.filter(user =>{
+   let [{name,address,company}] = UserData.filter(user =>{
        if(user.id == rowValue) return user;
     });
-    console.log('test',id, name, address,company)
 
     for (var card = 0; card < cardsElements.length; card++) {
       if (cardsElements[card].id== 'company'){
@@ -57,20 +85,10 @@ async function  loadDataCards(cardsElements,tableElement,mediaCardEl,dataUsers) 
             mediaCardEl[media].children[2].innerHTML = address.city
             mediaCardEl[media].children[3].innerHTML = address.zipcode
           }
-
         }
-
       }
     }
+  })
 
-  });
-
- }
-
- loadDataCards(cards,tbody,mediaCards,dataUsers)
-
-
-
-
-
-
+}
+filterUserbyClick(tbody,cards,mediaCards,dataUsers);
