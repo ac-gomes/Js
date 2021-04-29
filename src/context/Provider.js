1cf0e7f108a91cc9d54bc0a5456f36b42a038934
Antonio Gomes <60694694+ac-gomes@users.noreply.github.com>
2021-04-28 23:39:47 -0300

    init:Context

---
+++ src/context/Provider.js
@@ -0,0 +1,24 @@
+import React, {createContext, useReducer } from 'react';
+import authInitialState from './initialState/authState';
+import contactsInitialState from './initialState/contactsInitialState';
+import auth from './reducers/auth';
+import contacts from './reducers/contact';
+
+export const GlobalContext = createContext({});
+
+const GlobalProvider=({children}) => {
+  const [authState, authDispatch] = useReducer(auth, authInitialState);
+  const [contactsState, contactsDispatch] = useReducer(
+    contacts,
+    contactsInitialState,
+  );
+
+  return(
+    <GlobalContext.Provider value={
+      {authState, contactsState, authDispatch, contactsDispatch}}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export default GlobalProvider;
---
+++ src/screens/Contact/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Text, View } from 'react-native';
+
+const Contacts = () => {
+  return(
+  <View>
+    <Text>Hi from contacts</Text>
+  </View>
+  );
+};
+
+export default Contacts;
