3da725d937f074e710ea221c1c8ee82205d833ee
Antonio Gomes <60694694+ac-gomes@users.noreply.github.com>
2021-05-04 22:39:30 -0300

    Form validation

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -8,7 +8,12 @@ import Input from '../../components/Common/Input';
 import styles from './styles';
 import {LOGIN} from '../../constants/routeNames';
 
-const RegisterComponent = () => {
+const RegisterComponent = ({
+  onSubmit,
+  onChange,
+  form,
+  errors,
+}) => {
   const {navigate} = useNavigation();
     return(
     <Container>
@@ -18,42 +23,64 @@ const RegisterComponent = () => {
       />
 
       <View>
-        <Text style={styles.title}>Bem Vindo!</Text>
+        <Text style={styles.title}>Welcome to XYZ!</Text>
         <Text style={styles.subTitle}>Create a free account</Text>
 
         <View style={styles.form}>
 
           <Input
-          label='Username'
-          iconPosition ='right'
-          placeholder="Enter Username"
-          />
-          <Input
-          label='First name'
-          iconPosition ='right'
-          placeholder="Enter First name"
-          />
-
-          <Input
-          label='Last name'
-          iconPosition ='right'
-          placeholder='Enter Last name'
+            label='Username'
+            iconPosition ='right'
+            placeholder="Enter Username"
+            error={errors.userName}
+            onChangeText={(value)=>{
+              onChange({name: 'userName', value});
+            }}
           />
 
           <Input
-          label='Email'
-          iconPosition ='right'
-          placeholder="Enter Email"
+            label='First name'
+            iconPosition ='right'
+            placeholder='Enter First name'
+            error={errors.firstName}
+            onChangeText={(value)=>{
+            onChange({name: 'firstName', value});
+            }}
           />
 
           <Input
-          label='Password'
-          placeholder="Enter Password"
-          secureTextEntry={true}
-          icon={<Text>Show</Text>}
-          iconPosition ='right'/>
+            label='Last name'
+            iconPosition ='right'
+            placeholder='Enter Last name'
+            error={errors.lastName}
+            onChangeText={(value)=>{
+              onChange({name: 'lastName', value});
+            }}
+          />
 
-          <CustomButton primary title='Submit'/>
+          <Input
+            label='Email'
+            iconPosition ='right'
+            placeholder="Enter Email"
+            error={errors.email}
+            onChangeText={(value)=>{
+              onChange({name: 'email', value});
+            }}
+          />
+
+          <Input
+            label='Password'
+            placeholder="Enter Password"
+            secureTextEntry={true}
+            icon={<Text>Show</Text>}
+            iconPosition ='right'
+            error={errors.password}
+            onChangeText={(value)=>{
+              onChange({name: 'password', value});
+            }}
+          />
+
+          <CustomButton onPress={onSubmit} primary title='Submit'/>
 
           <View style={styles.createSection}>
             <Text style={styles.infoText}>Already have a account?</Text>
--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,11 +1,81 @@
 import React from 'react';
+import { useState } from 'react';
 
 // import {Text, View } from 'react-native';
 import RegisterComponent from '../../components/Signup';
 
 const Register = () => {
+  const [form, setForm] = useState({});
+  const [errors, setErrors] = useState({});
+
+  const onChange=({name, value}) =>{
+    setForm({...form, [name]: value });
+    //aqui são validados os valores dos campos
+    //verificar se a senha tem 6 caracters, melhorar isso ta orrivel
+    if(value !== ''){
+      if(name =='password'){
+        if(value.length < 6){
+          setErrors((prev) => {
+            return {...prev, [name]: 'This field needs min 6 caracters'}
+          });
+        }else{
+          setErrors((prev) => {
+            return {...prev, [name]: null}
+          });
+      }
+      }else{
+        setErrors((prev) => {
+          return {...prev, [name]: null}
+        });
+      }
+    }else{
+      setErrors((prev) => {
+        return {...prev, [name]: 'This field is riquired'}
+      });
+    }
+  };
+
+  const onSubmit = () =>{
+    //conditions
+    console.log('form >> ',form);
+    if(!form.userName){
+      setErrors((prev) => {
+        return {...prev, userName: 'Please add a username'}
+      });
+    }
+
+    if(!form.firstName){
+      setErrors((prev) => {
+        return {...prev, firstName: 'Please add a first name'}
+      });
+    }
+
+    if(!form.lastName){
+      setErrors((prev) => {
+        return {...prev, lastName: 'Please add a last name'}
+      });
+    }
+
+    if(!form.email){
+      setErrors((prev) => {
+        return {...prev, email: 'Please add a email'}
+      });
+    }
+
+    if(!form.password){
+      setErrors((prev) => {
+        return {...prev, password: 'Please add a passwor'}
+      });
+    }
+  };
+
   return(
-    <RegisterComponent/>
+    <RegisterComponent
+      onSubmit={onSubmit}
+      onChange={onChange}
+      form={form}
+      errors={errors}
+    />
   );
 };
 
